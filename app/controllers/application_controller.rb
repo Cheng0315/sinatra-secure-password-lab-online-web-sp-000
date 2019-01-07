8ce8b8f0066e3a266c6ca5a23c48423a813385e5
Cheng Vue <cvu3@ucsc.edu>
2019-01-07 10:09:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     @user = User.new(:username => params[:username], :password => params[:password])
-    if @user.save && @user[:user] != nil
+    if @user.save && @user[:username] != nil
       redirect "/login"
     else
       redirect "/failure"
